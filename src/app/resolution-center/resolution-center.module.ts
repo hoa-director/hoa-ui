c78cc003116db1fe84eb9538b45dc9590399701d
John Gellert <gellertjm@gmail.com>
2020-06-30 15:22:34 -0500

    update routing for resolution center

--- src/app/resolution-center/resolution-center.module.ts
+++ src/app/resolution-center/resolution-center.module.ts
@@ -1,36 +1,52 @@
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-// import { ModalComponent } from '../app/modal/modal.component';
-import { CreateObjectionComponent } from './create-objection/create-objection.component';
-import { InboxComponent } from './inbox/inbox.component';
-import { ObjectionComponent } from './objection/objection.component';
-import { ResolutionCenterComponent } from './resolution-center.component';
+import { CreateObjectionComponent } from "./create-objection/create-objection.component";
+import { InboxComponent } from "./inbox/inbox.component";
+import { ObjectionComponent } from "./objection/objection.component";
+import { ResolutionCenterComponent } from "./resolution-center.component";
 
-import { OutboxComponent } from './outbox/outbox.component';
-import { ResolutionCenterRoutingModule } from './resolution-center-routing.module';
-import { ResolutionCenterService } from './resolution-center.service';
-import { PastObjectionsComponent } from './past-objections/past-objections.component';
+import { OutboxComponent } from "./outbox/outbox.component";
+import { ResolutionCenterService } from "./resolution-center.service";
+import { PastObjectionsComponent } from "./past-objections/past-objections.component";
+
+import { RouterModule, Routes } from "@angular/router";
+import { AuthGuardService } from "../services/auth-guard.service";
+
+export const ResolutionCenterRoutes: Routes = [
+  {
+    path: "resolution-center",
+    component: ResolutionCenterComponent,
+    canActivate: [AuthGuardService],
+    children: [
+      { path: "", redirectTo: "main/resolution-center/inbox", pathMatch: "full" },
+      { path: "inbox", component: InboxComponent },
+      { path: "outbox", component: OutboxComponent },
+      { path: "past", component: PastObjectionsComponent },
+      { path: "objection/create", component: CreateObjectionComponent },
+      { path: "objection/view/:id", component: ObjectionComponent },
+      { path: "objection/:id", component: ObjectionComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule,
-    ResolutionCenterRoutingModule,
     FormsModule,
+    RouterModule.forChild(ResolutionCenterRoutes),
   ],
   declarations: [
     ResolutionCenterComponent,
     InboxComponent,
     ObjectionComponent,
     CreateObjectionComponent,
-    // ModalComponent,
     OutboxComponent,
     PastObjectionsComponent,
   ],
   providers: [ResolutionCenterService],
-  // exports: [ModalComponent],
 })
 export class ResolutionCenterModule {}
