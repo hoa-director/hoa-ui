fad2d6fb222e70ee50161f0dcc900ec93abd67d4
John Gellert <gellertjm@gmail.com>
2020-08-26 00:30:25 -0500

    added password strength meter

--- src/app/forgotten-password/forgotten-password.component.ts
+++ src/app/forgotten-password/forgotten-password.component.ts
@@ -1,25 +1,27 @@
 // import { Location } from '@angular/common';
-import {ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation, OnDestroy} from '@angular/core';
 import { UserService } from "../services/user.service";
 import { ActivatedRoute } from "@angular/router";
 import { Router } from "@angular/router";
 import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { BehaviorSubject, Subscription } from 'rxjs';
+import { response } from 'express';
 
 @Component({
   selector: "app-forgotten-password",
   templateUrl: "./forgotten-password.component.html",
   styleUrls: ["./forgotten-password.component.css"],
-  //encapsulation: ViewEncapsulation.None,
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ForgottenPasswordComponent implements OnInit {
+export class ForgottenPasswordComponent implements OnInit, OnDestroy {
   public token: string = "";
-  public message: string = "";
+  private responseMessageSubject = new BehaviorSubject<string>(null);
+  private messageSubs: Subscription;
+  public message: string;
   public isSuccess: boolean = false;
   public noSuccess: boolean = false
 
   resetPassword = new FormGroup({
-    password: new FormControl("", [Validators.required, ]),
+    password: new FormControl("", [Validators.required]),
   });
 
   constructor(
@@ -30,6 +32,15 @@ export class ForgottenPasswordComponent implements OnInit {
 
   ngOnInit() {
     this.token = this.route.snapshot.paramMap.get("token");
+    this.messageSubs = this.responseMessageSubject.subscribe(
+      (response) => {
+        this.message = response
+      }
+    );
+  }
+
+  ngOnDestroy(){
+    this.messageSubs.unsubscribe();
   }
 
   onCancel() {
@@ -46,12 +57,12 @@ export class ForgottenPasswordComponent implements OnInit {
       .subscribe(
         (response) => {
           this.isSuccess = true;
-          this.message = "Your password has been changed. Please login with you new password";
+          this.responseMessageSubject.next("Your password has been changed. Please login with you new password. You will be brought to the login page shortly...");
           setTimeout(() => {this.router.navigate(["/login"])}, 5000);
         },
         (error) => {
           this.noSuccess = true;
-          this.message = "There was a server error. Please try again later";
+          this.responseMessageSubject.next("There was a server error. Please try again later");
         }
       );
   }
