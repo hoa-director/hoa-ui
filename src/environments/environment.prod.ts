ac0a9a9b2f61a2c979d933909be7ac63dfed7e0d
Nate Notermann <98718660+NateNotermann@users.noreply.github.com>
2024-09-12 20:49:33 -0500

    added correct production URL to environment variable.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://hoa-director.herokuapp.com',
+  // apiUrl: 'https://hoa-director.herokuapp.com',
+  apiUrl: 'https://hoa-director.herokuapp.com', // -- Nate added, but did
 };
