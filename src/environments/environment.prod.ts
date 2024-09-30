85c56d6e69d2744dd32cb23c02a890381cb1150b
Nate Notermann <98718660+NateNotermann@users.noreply.github.com>
2024-09-29 21:40:50 -0500

    removed / from prod apiURL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://hoa-api-production.herokuapp.com/',
+  apiUrl: 'https://hoa-api-production.herokuapp.com',
   // apiUrl: 'https://hoa-director.herokuapp.com', // -- Nate added, but did not seem to fix cors errors.
 };
