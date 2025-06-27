60dd6c01b9884a16587a68905fab43180ebc9820
Liz Kerber <lizkerber12@gmail.com>
2025-06-26 21:15:39 -0500

    User Center home: Get all users from all associations

--- src/app/services/users-center.service.ts
+++ src/app/services/users-center.service.ts
@@ -43,15 +43,9 @@ export class UsersCenterService {
 
   // --  GET ALL USERS
   fetchUsers(inputString: string) { 
-    const endPoint = "/api/fetchUsers"
-      // .set('associationId',sessionStorage.getItem("associationId").toString()) // -- get from session
-      // .set('associationId', associationId.toString())  // -- get from previous function
-      const associationId = sessionStorage.getItem("associationId").toString()
-      const payload = {
-            associationId: [associationId], // -- associationIds MUST be un an array to work.
-            inputString: inputString
-          }
-    return this.http.post(BACKEND_URL + endPoint, payload );
+    const payload = { inputString: inputString || '' };
+    const endPoint = "/api/fetchUsers";
+    return this.http.post(BACKEND_URL + endPoint, payload);
   }
 
 
